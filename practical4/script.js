e4c3edbab247f701940785f3b55eedb897cc4a62
Keelan <keelanmatthews123@gmail.com>
2022-08-26 07:44:49 +0200

    finished section A

---
+++ practical4/script.js
@@ -0,0 +1,75 @@
+class FactorialChecker {
+	printFactorial(num) {
+		let fact = 1;
+		for (let i = 1; i <= num; i++) {
+			fact *= i;
+		}
+		return fact;
+
+	}; //end function
+
+	fillArray(num) {
+		let start = num;
+		let init = 1;
+		let i = 1;
+		let arr = [];
+		let fac = true;
+		while (true) {
+			init = init * i;
+			if (init % 2 == 0 || init == 1) {
+				if (init > start) {
+					fac = false;
+					break;
+				}
+				arr.push(i);
+				i++;
+				if (init == start)
+					break;
+			}
+		}
+		if (fac)
+			return arr;
+		else
+			return "This is not a factorial";
+	}; //end function
+}//end FactorialChecer
+
+
+const PigLatinEncrypt = str =>
+{
+	let vowels = "aeiou";
+	vowels = vowels.split("");
+	let newStr = [];
+	str = str.toLowerCase();
+
+    if (vowels.indexOf(str[0]) > -1) {
+        newStr = str + "way";
+        return newStr;
+    } else {
+        let firstMatch = str.match(/[aeiou]/g) || 0;
+        let vowel = str.indexOf(firstMatch[0]);
+        newStr = str.substring(vowel) + str.substring(0, vowel) + "ay";
+        return newStr;
+    }
+
+}//end PigLatinEncrypt
+
+
+const checker = new FactorialChecker();
+
+document.getElementById("GenerateFactorial").onclick = () =>
+{
+	let input = document.getElementById("FactorialValue").value;
+	document.getElementById("FactorialResult").innerHTML = `The factorial value of ${input} is ${checker.printFactorial(input)}`;
+}
+
+document.getElementById("GenerateFactorialList").onclick = () =>
+{
+	let input = document.getElementById("FactorialInput").value
+	document.getElementById("FactorialListResult").innerHTML = `The values that make up the factorial ${input}: ${checker.fillArray(input)}`;
+}
+
+document.getElementById("PigLatinEncrypt").onclick = () =>
+{
+	document.getElementById("PigLatinResult").innerHTML = PigLatinEncrypt(document.getElementById("SentenceToConvert").value);
+}
