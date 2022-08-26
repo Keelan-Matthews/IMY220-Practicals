ca70939a04d308272253493c2fa6c00104ebe16b
Keelan <keelanmatthews123@gmail.com>
2022-08-26 07:53:05 +0200

    finished

--- practical4/script.js
+++ practical4/script.js
@@ -35,41 +35,53 @@ class FactorialChecker {
 }//end FactorialChecer
 
 
-const PigLatinEncrypt = str =>
-{
+const PigLatinEncrypt = str => {
 	let vowels = "aeiou";
 	vowels = vowels.split("");
 	let newStr = [];
 	str = str.toLowerCase();
 
-    if (vowels.indexOf(str[0]) > -1) {
-        newStr = str + "way";
-        return newStr;
-    } else {
-        let firstMatch = str.match(/[aeiou]/g) || 0;
-        let vowel = str.indexOf(firstMatch[0]);
-        newStr = str.substring(vowel) + str.substring(0, vowel) + "ay";
-        return newStr;
-    }
+	if (vowels.indexOf(str[0]) > -1) {
+		newStr = str + "way";
+		return newStr;
+	} else {
+		let firstMatch = str.match(/[aeiou]/g) || 0;
+		let vowel = str.indexOf(firstMatch[0]);
+		newStr = str.substring(vowel) + str.substring(0, vowel) + "ay";
+		return newStr;
+	}
 
 }//end PigLatinEncrypt
+
+const checkUniqueLetters = str => {
+	let arr = str.split("");
+	const unique = arr.reduce(
+		(acc, curr) =>
+			acc.find((i) => i === curr) ? acc : [...acc, curr],
+		[]
+	);
+	return unique.length;
+}
 
 
 const checker = new FactorialChecker();
 
-document.getElementById("GenerateFactorial").onclick = () =>
-{
+document.getElementById("GenerateFactorial").onclick = () => {
 	let input = document.getElementById("FactorialValue").value;
 	document.getElementById("FactorialResult").innerHTML = `The factorial value of ${input} is ${checker.printFactorial(input)}`;
 }
 
-document.getElementById("GenerateFactorialList").onclick = () =>
-{
+document.getElementById("GenerateFactorialList").onclick = () => {
 	let input = document.getElementById("FactorialInput").value
 	document.getElementById("FactorialListResult").innerHTML = `The values that make up the factorial ${input}: ${checker.fillArray(input)}`;
 }
 
-document.getElementById("PigLatinEncrypt").onclick = () =>
-{
-	document.getElementById("PigLatinResult").innerHTML = PigLatinEncrypt(document.getElementById("SentenceToConvert").value);
+document.getElementById("PigLatinEncrypt").onclick = () => {
+	let input = document.getElementById("SentenceToConvert").value;
+	document.getElementById("PigLatinResult").innerHTML = `The result when converting ${input} to pig latin is: ${PigLatinEncrypt(input)}`;
+}
+
+document.getElementById("checkRecurringChars").onclick = () => {
+	let input = document.getElementById("charChecker").value;
+	document.getElementById("recurringCharsResult").innerHTML = `The word ${input} has ${checkUniqueLetters(input)} unique letters`;
 }
