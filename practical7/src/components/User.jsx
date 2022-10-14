642f0e6406db99a53eb740513b8a6df608f486f7
Keelan <keelanmatthews123@gmail.com>
2022-10-14 08:13:44 +0200

    Finished p7

--- practical7/src/components/User.jsx
+++ practical7/src/components/User.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 export default class User extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {showBody: false};
+        this.toggleBody = this.toggleBody.bind(this);
+    }
+
+    toggleBody() {
+        this.setState({showBody: !this.state.showBody});
+    }
+
     render() {
         return (
             <div className="col-6">
-                <div className="card">
+                <div className="card" onClick={this.toggleBody}>
                     <div className="card-header">
-                        <p>{this.props.user.username}</p>
+                        {this.props.user.username}
                     </div>
-                    <div className="card-body">
+                    <div className={`card-body ${this.state.showBody ? '' : 'd-none'}`}>
                         <p className="card-text"><span className="fw-bold">Name: </span>{this.props.user.name}</p>
                         <p className="card-text"><span className="fw-bold">Surname: </span>{this.props.user.surname}</p>
                         <p className="card-text"><span className="fw-bold">Age: </span>{this.props.user.age}</p>
--- practical7/src/script.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello world!');
-const card = document.querySelectorAll('.card');
-
-card.forEach((card) => {
-  card.addEventListener('click', '.card-body', () => {
-    card.classList.toggle('d-none');
-  });
-});
